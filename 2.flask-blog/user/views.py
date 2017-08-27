df08ba2513f5d063f3f7846210d55b1de085b835
Lin <abigeater@163.com>
2017-08-27 17:26:55 +0800

    update paging show~ blog can use in now

--- 2.flask-blog/user/views.py
+++ 2.flask-blog/user/views.py
@@ -3,20 +3,32 @@ from flask import render_template,request
 from user.sqlite import Essay
 
 @app.route('/')
-@app.route('/page/<page>')
-@app.route('/class/<essay_class>')
-@app.route('/class/<essay_class>/page/<page>')
+@app.route('/page/<page>/')
+@app.route('/class/<essay_class>/')
+@app.route('/class/<essay_class>/page/<page>/')
 def index(page=1,essay_class='all'):
-    essays = Essay.query.order_by(Essay.id.desc()).limit(5)
     phpamount = Essay.query.filter_by(essay_class='php').count()
     pythonamount = Essay.query.filter_by(essay_class='python').count()
     allamount = Essay.query.count()
+    if essay_class == 'all':
+        essays = Essay.query.order_by(Essay.id.desc()).paginate(int(page), 5, False).items
+        page_number = int(allamount / 5)+1
+    elif essay_class == 'php':
+        essays = Essay.query.filter_by(essay_class='php').order_by(Essay.id.desc()).paginate(int(page), 5, False).items
+        page_number = int(phpamount / 5)+1
+    elif essay_class == 'python':
+        page_number = int(pythonamount / 5)+1
+        essays = Essay.query.filter_by(essay_class='python').order_by(Essay.id.desc()).paginate(int(page), 5, False).items        
+
+    
     return render_template('index.html', 
                             Essays = essays,
                             allamount = allamount,
                             phpamount = phpamount,
                             pythonamount = pythonamount,
-                            choose = essay_class )
+                            choose = essay_class,
+                            page_number = range(page_number)
+                            )
 
 @app.route('/essay/<essay_id>')
 def essay(essay_id):
