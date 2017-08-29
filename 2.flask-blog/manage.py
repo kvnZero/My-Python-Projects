decf025f3f944ef691ac6b8861c3f5ae0cde3586
Lin <abigeater@163.com>
2017-08-29 17:20:17 +0800

    update write essay page and markdown show

--- 2.flask-blog/manage.py
+++ 2.flask-blog/manage.py
@@ -1,7 +1,8 @@
 from user import app
 from flask_script import Manager
+from flaskext.markdown import Markdown
 manager = Manager(app)
-
+Markdown(app)
 if __name__ == '__main__':
     app.run(debug=True)
     #manager.run()
--- 2.flask-blog/user/views.py
+++ 2.flask-blog/user/views.py
@@ -1,12 +1,27 @@
 from user import app
 from flask import render_template,request
+from flask_wtf import Form
+from wtforms import TextAreaField, SelectField
+from wtforms import StringField, SubmitField
+from wtforms.validators import Required
 from user.sqlite import Essay
 import re
+
+app.config['SECRET_KEY'] = "abigeater"
+app.config['Admin_username'] = "abigeater"
+app.config['Admin_password'] = "xusong"
+
+class EssayForm(Form):
+    essay_title =  StringField('文章标题', validators=[Required()])
+    essay_summary = TextAreaField('文章摘要')
+    essay_class = SelectField('文章分类', choices=[('other','不分类'),('python','python'),('php','php')])
+    essay_content = TextAreaField('文章内容')
+    submit = SubmitField('发布文章')
 
 @app.template_filter('change_summary')
 def change_summary(l):
     content = re.sub('(<img.+?>)', '[图片]',l)
-    return content[0:126]
+    return content[0:63]
 
 @app.route('/')
 @app.route('/page/<page>/')
@@ -40,3 +55,21 @@ def index(page=1,essay_class='all'):
 def essay(essay_id):
     essay = Essay.query.filter_by(id=essay_id).first()
     return render_template ('essay.html',essay = essay)
+
+@app.route('/admin/<username>/<password>/')
+def admin(username,password):
+    if username != app.config['Admin_username'] or password != app.config['Admin_password']:
+        return "Error Get.!"
+    else:
+        return render_template('adminuse.html')
+
+@app.route('/admin/<username>/<password>/pushessay/', methods=['GET', 'POST'])
+def pushessay(username,password):
+    if username != app.config['Admin_username'] or password != app.config['Admin_password']:
+        return "Error Get.!"
+    else:
+        form = EssayForm()
+        return_content = ""
+        if form.essay_title.data != None:
+            name = form.essay_title.data
+        return render_template('pushessay.html',form = form, return_content = return_content)
