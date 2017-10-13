0546aea189970ea61dc7de7fe9410abcc46105a4
Lin <abigeater@163.com>
2017-10-13 18:50:24 +0800

    update all

--- 7.flask-short-url/shorturl/views.py
+++ 7.flask-short-url/shorturl/views.py
@@ -4,13 +4,17 @@ from flask_wtf import FlaskForm
 from wtforms.validators import InputRequired
 from wtforms import StringField, SubmitField
 from shorturl.models import Urls, db
-
+import random
 
 class urlform(FlaskForm):
-
     longurl = StringField(validators=[InputRequired()])
     submit = SubmitField('create short url')
 
+def randomkey():
+    chars = 'AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz0123456789'
+    a = list(chars)
+    random.shuffle(a)
+    return ''.join(a[:6])
 
 @app.route("/",methods=['GET','POST'])
 def index():
@@ -19,15 +23,24 @@ def index():
         long_url = form.longurl.data
         data = Urls.query.filter_by(url=long_url)
         if data.count() != 0:
-            return render_template('index.html', title="create short url", form=form, shorturl=data.first().key)
+            return render_template('index.html', title="create short url", form=form, urlkey=data.first().key)
         else:
-            new_url = Urls(url=long_url, key="hello")
+            key = randomkey()
+            keycount = Urls.query.filter_by(key=key).count()
+            while keycount != 0:
+                key = randomkey()
+                keycount = Urls.query.filter_by(key=key).count()
+            new_url = Urls(url=long_url, key=key)
             db.session.add(new_url)
             db.session.commit()
-            return render_template('index.html', title="create short url", form=form, shorturl=data.first().key)
+            return render_template('index.html', title="create short url", form=form, urlkey=data.first().key)
     return render_template('index.html', title="create short url", form=form)
 
-@app.route("/<urlkey>")
+@app.route("/s/<urlkey>")
 def backurl(urlkey):
     data = Urls.query.filter_by(key=urlkey)
     return redirect(data.first().url)
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return redirect('/')
